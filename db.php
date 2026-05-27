96efc9a57efc7b990fb9af07b347498612907370
ariharasutha <ariharasuthan685@gmail.com>
2026-05-27 21:19:31 +0530

    Update db.php

--- db.php
+++ db.php
@@ -1,8 +1,16 @@
 <?php
  
- $conn=new mysqli('localhost','root','','project');
- if($conn->connect_error){
-    die("connection failed". $conn->connect_error);
- }
+$conn=new mysqli(
+'localhost',
+'root',
+'',
+'project'
+);
+
+if($conn->connect_error){
+
+die("Connection Failed ".$conn->connect_error);
+
+}
 
 ?>
