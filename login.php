29a7dce6958df67d115fb902d03de05b38a7df13
ariharasutha <ariharasuthan685@gmail.com>
2026-05-27 21:13:35 +0530

    Enhance login functionality and form layout
    Refactor login logic to set session on successful login and improve form structure.

--- login.php
+++ login.php
@@ -1,28 +1,72 @@
 <?php
-session_start();
-if(isset($_POST['login'])){
-    $email=$_POST['email'];
-    $password=$_POST['password'];
-    $sql="SELECT*FROM user where email='$email' and password='$password'";
-    $result=$conn->query($sql);
 
-    if($result->num_rows>0){
-  
-        echo "login success";
-        header("Location:index.php");
-    }
-    else{
-        echo "invailed login";
-    }
+session_start();
+
+include "db.php";
+
+if(isset($_POST['login'])){
+
+$email=$_POST['email'];
+$password=$_POST['password'];
+
+$sql="SELECT * FROM user 
+WHERE email='$email' 
+AND password='$password'";
+
+$result=$conn->query($sql);
+
+if($result->num_rows > 0){
+
+$_SESSION['user']=$email;
+
+header("Location:index.php");
+exit();
+
+}else{
+
+echo "Invalid Login";
+
 }
+
+}
+
 ?>
+
 <!DOCTYPE html>
+<html>
+
+<head>
+
+<title>Login</title>
+
+</head>
+
 <body>
-    <form method="POST">
-EMAIL:<input type="email" id="email" name="email" placeholder="Enter Email"><br>
-PASSWORD<input type="password" id="password" name="password" placeholder="Enter password"><br>
-<button type="submit" name="login">login</button>
-</form>   
+
+<form method="POST">
+
+EMAIL:
+<input type="email"
+id="email"
+name="email"
+placeholder="Enter Email">
+
+<br><br>
+
+PASSWORD:
+<input type="password"
+id="password"
+name="password"
+placeholder="Enter Password">
+
+<br><br>
+
+<button type="submit" name="login">
+Login
+</button>
+
+</form>
+
 </body>
 
 </html>
