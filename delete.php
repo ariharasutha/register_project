01f6678581bf167afb6e92dae4c38818a3ce84a5
ariharasutha <ariharasuthan685@gmail.com>
2026-05-27 21:18:21 +0530

    Refactor delete.php for improved error handling

--- delete.php
+++ delete.php
@@ -1,14 +1,20 @@
 <?php
-$id=isset($_GET['id'])?intval($_GET['id']):0;
-$sql=$conn->query("DELETE FROM user where id=$id");
+
+include "db.php";
+
+$id=isset($_GET['id']) ? intval($_GET['id']) : 0;
+
+$sql="DELETE FROM user WHERE id=$id";
+
 if($conn->query($sql)){
-    header("Location:index.php");
-    exit;
-}
-else{
-    echo "error".$conn->error;
-}
 
+header("Location:index.php");
+exit();
 
+}else{
+
+echo "Error ".$conn->error;
+
+}
 
 ?>
